0e2f0455ace58d957deb7404571edc6041eea6bd
Joe Neuman <Joe Neuman>
2015-12-27 08:36:51 -0700

    finished

--- js/controller.js
+++ js/controller.js
@@ -1,8 +1,31 @@
-angular.module('myApp').controller('mainControl',
-    function ($scope) {
-        $scope.name = "Controller & App Conected";
+angular.module('myApp').controller('mainControl', function ($scope) {
+    $scope.test = "Donzo"
+    $scope.niceList = [];
+    $scope.naughtyList = [];
 
+    $scope.addNewPerson = function () {
+        var differenceScore = $scope.niceScore - $scope.naughtyScore;
 
+        if (differenceScore >= 10) {
+            $scope.niceList.push(buildPerson(differenceScore));
+        } else {
+            $scope.naughtyList.push(buildPerson(differenceScore));
+        }
+    };
 
-
-    });
+    function buildPerson(differenceScore) {
+        return {
+            name: $scope.name,
+            score: differenceScore
+        };
+    }
+    
+    /*
+         {
+             name: 'Luke Skywalker',
+             niceScore: (0-100)
+             naughtyScore: (0-100)
+             
+         }
+    */
+});
